0ae473f1d6612aa950419b942e0ab0c4908ff305
edwin <edwin@mediamonks.com>
2019-03-12 07:39:21 +0100

    added base adapter test

---
+++ adapter/adapter_test.go
@@ -0,0 +1,31 @@
+package adapter
+
+import (
+	"testing"
+)
+
+func TestBaseAdapter_SetPathPrefix(t *testing.T) {
+	a := &BaseAdapter{}
+	a.SetPathPrefix("data")
+
+	if *a.pathPrefix != "data/" {
+		t.Log("unexpected path prefix")
+		t.Fail()
+	}
+}
+
+func TestBaseAdapter_ApplyPathPrefix(t *testing.T) {
+	a := &BaseAdapter{}
+	a.SetPathPrefix("data")
+	p := a.ApplyPathPrefix("sub")
+
+	if *a.pathPrefix != "data/" {
+		t.Log("unexpected path prefix")
+		t.Fail()
+	}
+
+	if p != "data/sub" {
+		t.Log("unexpected path")
+		t.Fail()
+	}
+}
